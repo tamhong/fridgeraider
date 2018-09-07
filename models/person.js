12ebacee7df188fa16855687e6502b5c17501744
Tam Hong <tamhong.sd@gmail.com>
2018-09-06 21:31:43 -0700

    format fixes and some front end

--- models/person.js
+++ models/person.js
@@ -1,42 +1,43 @@
-module.exports = function(sequelize, DataTypes) {
-  var Person = sequelize.define("Person", {
-  	id: {
-  		type: DataTypes.INTEGER,
-  		primaryKey: true,
-      allowNull: false,
-  		autoIncrement: true
-  	},
-  	username: {
-  		type: DataTypes.STRING,
-  		allowNull: false,
-  		required: true
-  	},
-  	email: {
-  		type: DataTypes.STRING,
-  		allowNull: false,
-  		required: true
-  	},
-  	phone_number: {
-  		type: DataTypes.INTEGER,
-  		allowNull: false
-  	},
-  	fridge_name: {
-  		type: DataTypes.STRING,
-  		allowNull: false,
-  		required: true
-  	}
-  },
-  {
-  	tableName: "Person"
-  });
+module.exports = function (sequelize, DataTypes) {
+	var Person = sequelize.define(
+		"Person", {
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				allowNull: false,
+				autoIncrement: true
+			},
+			username: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				required: true
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				required: true
+			},
+			phoneNumber: {
+				type: DataTypes.INTEGER,
+				allowNull: false
+			},
+			fridgeName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				required: true
+			}
+		},
+		{
+			tableName: "Person"
+		});
 
 
-  Person.associate = function(models) {
+	Person.associate = function (models) {
 
-    Person.hasMany(models.Items, {
-      onDelete: "cascade"
-    });
-  };
+		Person.hasMany(models.Items, {
+			onDelete: "cascade"
+		});
+	};
 
-  return Person;
+	return Person;
 };
