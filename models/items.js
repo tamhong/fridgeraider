4245ee1426d4eb53b0016233a9adb9d403d2e45b
koneone2002 <katie@imagings-MacBook-Pro.local>
2018-09-13 20:50:23 -0700

    cleaned up code

--- models/items.js
+++ models/items.js
@@ -1,59 +1,58 @@
-module.exports = function (sequelize, DataTypes) {
-	var Items = sequelize.define(
-		"Items", {
-			id: {
-				type: DataTypes.INTEGER,
-				primaryKey: true,
-				allowNull: false,
-				autoIncrement: true
-			},
-			itemName: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				required: true
-			},
-			category: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				required: true
-			},
-			expDate: {
-				type: DataTypes.DATEONLY,
-				allowNull: false,
-				required: true
-			},
-			createdAt: {
-				type: DataTypes.DATE,
-				allowNull: false,
-				defaultValue: DataTypes.NOW
-			},
-			hasExpired: {
-				type: DataTypes.BOOLEAN,
-				allowNull: false,
-				required: true,
-				defaultValue: false
-			},
-			used: {
-				type: DataTypes.BOOLEAN,
-				allowNull: false,
-				required: true,
-				defaultValue: false
-			}
-		}, {
+module.exports = function(sequelize, DataTypes) {
+  var Items = sequelize.define(
+    "Items",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      itemName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+      },
+      expDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        required: true
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      },
+      hasExpired: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        required: true,
+        defaultValue: false
+      },
+      used: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        required: true,
+        defaultValue: false
+      }
+    },
+    {
+      tableName: "Items"
+    }
+  );
 
+  Items.associate = function(models) {
+    Items.belongsTo(models.Person, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
 
-			tableName: "Items"
-		});
-
-	Items.associate = function (models) {
-
-		Items.belongsTo(models.Person, {
-			foreignKey: {
-				allowNull: false
-			}
-		});
-	};
-
-	return Items;
+  return Items;
 };
-
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,23 @@
 var db = require("../models");
-var moment = require('moment');
+var moment = require("moment");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     // db.Items.findAll({}).then(function(dbItems) {
     res.render("index");
   });
-  app.get("/login", function (req, res) {
+  app.get("/login", function(req, res) {
     // db.Items.findAll({}).then(function(dbItems) {
     res.render("login");
   });
 
-  app.get("/signup", function (req, res) {
+  app.get("/signup", function(req, res) {
     // db.Items.findAll({}).then(function(dbItems) {
     res.render("signup");
   });
 
-  app.get("/recipe", function (req, res) {
+  app.get("/recipe", function(req, res) {
     // db.Items.findAll({}).then(function(dbItems) {
     res.render("recipe");
   });
@@ -40,37 +40,31 @@ module.exports = function (app) {
   //     }
   //   );
 
-  app.get("/fridge/:username", function (req, res) {
-
+  app.get("/fridge/:username", function(req, res) {
     var user = req.params.username;
 
     db.Person.findOne({
       where: { username: user },
-      attributes: ['id', 'fridgeName', 'username']
-    }).then(
-      function (dbPerson) {
-        if (dbPerson) {
-          db.Items.findAll({
-            where: { PersonId: dbPerson.id }
-          }).then(
-            function (dbItems) {
-              var hbsObject = {
-                items: dbItems,
-                userInfo: dbPerson.dataValues
-              };
+      attributes: ["id", "fridgeName", "username"]
+    }).then(function(dbPerson) {
+      if (dbPerson) {
+        db.Items.findAll({
+          where: { PersonId: dbPerson.id }
+        }).then(function(dbItems) {
+          var hbsObject = {
+            items: dbItems,
+            userInfo: dbPerson.dataValues
+          };
 
-              console.log(hbsObject);
+          console.log(hbsObject);
 
-              res.render("fridge", hbsObject);
-            }
-          )
-        } else {
-          res.status(400);
-          return res.json({ Error: "Username does not exist!" })
-        }
+          res.render("fridge", hbsObject);
+        });;
+      } else {
+        res.status(400);
+        return res.json({ Error: "Username does not exist!" });
       }
-    );
-
+    });
   });
 
   // Load example page and pass in an example by id
@@ -83,7 +77,7 @@ module.exports = function (app) {
   // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
