32205360234347d290ca91863febd0a87b0609ad
ekhin <waikhin10@gmail.com>
2018-09-14 01:20:29 -0700

    working Heroku

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "production") {
   syncOptions.force = true;
 }
 
